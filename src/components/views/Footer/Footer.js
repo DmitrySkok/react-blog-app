dd5a6bd5b58dc7e05d92cb175dabd281b0a482fe
Dmitry Skok <dimas.skok95@gmail.com>
2022-03-23 19:02:57 +0100

    Complete NavBar, Header, Footer

---
+++ src/components/views/Footer/Footer.js
@@ -0,0 +1,8 @@
+
+const Footer = () => {
+  return (
+    <div className='text-center text-muted mt-4 mb-4'>Copyright © BlogApp 2022</div>
+  );
+}
+
+export default Footer;
---
+++ src/components/views/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import { Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <Navbar bg="primary" variant="dark" expand="lg" className="mt-4 mb-4 rounded">
+      <Navbar.Brand className="me-auto" as={NavLink} to="/">Blog.app</Navbar.Brand>
+      <Nav>
+        <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+        <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+      </Nav>
+    </Navbar>
+  );
+}
+
+export default NavBar;
